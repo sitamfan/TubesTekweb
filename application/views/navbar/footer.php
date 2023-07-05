10fe8f3ea8dcd10500043dbbe98359107ca752cb
ayunuroktaviani16 <116129689+ayunuroktaviani16@users.noreply.github.com>
2023-07-05 08:39:16 +0700

    delete
    mengahpus yang sebelumnya

--- application/views/navbar/footer.php
+++
@@ -1,6 +0,0 @@
-        <!-- Bootstrap core JS-->
-        <script src="<?= site_url('assets/');?>https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="<?= site_url('assets/');?>js/scripts.js"></script>
-    </body>
-</html>
